21fe13be957b558cea9c231979be5bbe171db711
root <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>
2022-08-11 05:32:48 -0700

    added pint cant compile yet

--- Functions.c
+++ Functions.c
@@ -64,3 +64,25 @@ void pall(stack_t **stack, unsigned int line_number)
 	}
 	temp = NULL;
 }
+
+/**
+ * pop - removes the top element of the stack
+ * @stack: the stack
+ * @line_number: instruction line number
+ * Return: nothing
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *save;
+
+	if (!*stack)
+	{
+		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line_number);
+		argument[0] = "FAIL";
+		return;
+	}
+	save =(**stack)->next;
+	free(**stack);
+	**stack = save;
+}
